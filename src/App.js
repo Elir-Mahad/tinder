cf04aa52b80867bd4f450201231ffd6ce87cc825
code <mmahad280@gmail.com>
2020-09-07 17:35:59 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
 	return (
-		<div className="App">
+		<div className="app">
 			<h1>Kitty season</h1>
+
+			<Header />
+			{/* tinder cards */}
+			{/* buttons below tinder cards */}
+
+			{/* chat screen */}
+			{/* individual chat screen */}
 		</div>
 	);
 }
