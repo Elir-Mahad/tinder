8964a837e801896cc352fad1fdaace8384e8ca1e
code <mmahad280@gmail.com>
2020-09-07 17:42:04 -0400

    added materiaul ui icons

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Header.css";
 
+import PersonIcon from "@material-ui/icons/Person";
+
 function Header() {
 	return (
 		<div className="header">
+			<PersonIcon />
 			<h1>header</h1>
 		</div>
 	);
